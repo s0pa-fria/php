ab2012fa91bc2316f8d3dfbc9f1ea25747ef6e03
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-08-10 11:23:36 -0300

    sintaxe1

---
+++ Aula01/sintaxe1.php
@@ -0,0 +1,8 @@
+<?php
+    // Geração de texto (string) 
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estrutural (com tags, atributos) */
+    echo "<h2>Gerado <span class='destaque'>HTML</span> através do PHP.</h2>";
+    echo "<h2>Gerado <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+?>
